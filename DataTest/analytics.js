9bae920dffee909aa53d92cb6fa058833b60b072
Mikhail Okrugov <s0s100@mail.ru>
2025-01-27 17:03:22 +0300

    Removed analytics

--- DataTest/analytics.js
+++
@@ -1,6 +0,0 @@
-import telegramAnalytics from '@telegram-apps/analytics';
-
-telegramAnalytics.init({
-    token: 'eyJhcHBfbmFtZSI6IkJpelR5Y29vbiIsImFwcF91cmwiOiJodHRwcy8vdC5tZS9iaXp0eWNvb25fYm90IiwiYXBwX2RvbWFpbiI6Imh0dHBzOi8vYml6dHljb29uZ2FtZS5jb20vIn0=!fQGzXhnIbt6QG8Zgh4xgORjCUO2+qcWCiSbinwo+4Vg=',
-    appName: 'BizTycoon',
-});
