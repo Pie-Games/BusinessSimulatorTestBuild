cf9dde3adac6015d5f7fec58557d9f9f6fdee51b
Baste <assasinadyg@gmail.com>
2025-03-26 01:29:26 +0300

    Revert "Hash file names and data caching"
    This reverts commit 253c63084b4f04abef70ee679aed1dacaa912a8a.

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/9853637125e801e9aae48e78dbbdcfca.loader.js",
-    "Build/5e6163fd799a7dfba11f0a5f65eb33e6.framework.js.unityweb",
-    "Build/db3369ca842c52db3ee60bff2dcc9244.data.unityweb",
-    "Build/33bc1233229ca5303a3b5656d7194595.wasm.unityweb",
+    "Build/motchiBuild.loader.js",
+    "Build/motchiBuild.framework.js.unityweb",
+    "Build/motchiBuild.data.unityweb",
+    "Build/motchiBuild.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
