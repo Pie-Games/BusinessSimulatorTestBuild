ed292005f5a0fbee3c3e8b706dceaf38f09ced83
Sparkah <timopro16@gmail.com>
2025-03-28 20:59:12 +0500

    test new build pipeline +partners + ads bug fix

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "DefaultCompany-motchi-1.0";
-const contentToCache = [
-    "Build/9853637125e801e9aae48e78dbbdcfca.loader.js",
-    "Build/eb539cade0602602ba103fd5d741f216.framework.js.unityweb",
-    "Build/8d5e8060751ba3c50397109c503db8d6.data.unityweb",
-    "Build/d13044e9fc2b21222eac03c7d953a517.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
