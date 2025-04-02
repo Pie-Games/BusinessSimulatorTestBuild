5e63d38b1044aaccdeffc13adca3ab15fb13c10f
Baste <assasinadyg@gmail.com>
2025-04-02 17:55:43 +0300

    Revert "bundle test"
    This reverts commit 2b5ed49667528e19fc30b2a115f6c9f650fc8ef3.

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,12 +1,3 @@
-const cacheName = "DefaultCompany-motchi-1.0";
-const contentToCache = [
-    "Build/9853637125e801e9aae48e78dbbdcfca.loader.js",
-    "Build/2ea9b2ccd35ca45f2d188734675e57aa.framework.js.unityweb",
-    "Build/ceca27e9027981413a806b18761e48a3.data.unityweb",
-    "Build/4f80a054d9b0af07801c4fe4f262812f.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
