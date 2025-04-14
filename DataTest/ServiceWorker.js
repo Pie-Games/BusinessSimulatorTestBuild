136b588c6b37c087593f011825e83b83b8583754
Baste <assasinadyg@gmail.com>
2025-04-14 15:35:12 +0300

    richads test 5

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/bf9bc2ec6f453b3945bc441e20206ece.loader.js",
+    "Build/12eff2db9892d685b57b71d48a93ce53.loader.js",
     "Build/757023d57ff654fbec58846abfd79722.framework.js.br",
-    "Build/5583e1bf9eae6da0cdb9ad499bd39199.data.br",
-    "Build/333d95111cb52132449650ac681e1cd3.wasm.br",
+    "Build/a5804759af58e6031e3ebce4281e7e5b.data.br",
+    "Build/cc40a914d9dcbb0b7642e2cb4d59603f.wasm.br",
     "TemplateData/style.css"
 
 ];
