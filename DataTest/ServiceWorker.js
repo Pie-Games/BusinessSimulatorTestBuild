e59e44971e2368d82cfcca7dd6ff8e1532cbe2c1
Baste <assasinadyg@gmail.com>
2025-04-14 14:33:34 +0300

    richads increment test 2

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,7 +1,7 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
     "Build/9465a253a26a1f465c222155f553696d.loader.js",
-    "Build/04427ceda7494ca7260cd9e8b881f31f.framework.js.br",
+    "Build/757023d57ff654fbec58846abfd79722.framework.js.br",
     "Build/53c028306de21a3a482f726f28997cf4.data.br",
     "Build/15516eae155acbd56f9371deed11d8fa.wasm.br",
     "TemplateData/style.css"
