455e3a272fbeb38f6f1c4f58338e04ad41cf3ecb
Baste <assasinadyg@gmail.com>
2025-05-27 13:06:48 +0300

    Fix tutorial

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/13de11c6f98c2d2cb46fd2969f11bd59.loader.js",
-    "Build/b627c53cc61016841f25d363258b36c6.framework.js.br",
-    "Build/2759747ca6a00de39347437ac6ba440f.data.br",
-    "Build/fff43e91714cf8280cbe3133e137e8a9.wasm.br",
+    "Build/b7c832fa38a2969eee8c250865f28a3c.loader.js",
+    "Build/0a74755f3bdf7ed356dbeacd38f15fc4.framework.js.br",
+    "Build/7d29973086e453fbefdfaad09dc00da0.data.br",
+    "Build/12ff6236c80aafd60547c17c4b21b34d.wasm.br",
     "TemplateData/style.css"
 
 ];
