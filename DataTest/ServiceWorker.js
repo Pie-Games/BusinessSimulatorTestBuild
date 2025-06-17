a85b4283d15b4ead102c1b7f527a8f4b65c42f1c
Baste <assasinadyg@gmail.com>
2025-06-17 10:08:16 +0300

    Revert "wallet test 2"
    This reverts commit 6295aa87870e72bf985b72e3589d08ec00d10c2a.

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/158087c69a47af159acf1ec0991c8b79.loader.js",
+    "Build/bae518cb058674c741a3a1b68f79a9de.loader.js",
     "Build/7d4d1fa1eb0daf20c168abd55de46cb0.framework.js.br",
-    "Build/2667845623011f32da380c8cd01e1a77.data.br",
-    "Build/188d9a167e535e2659f693ca3ca72996.wasm.br",
+    "Build/20877568bfd8837e77e219b2deb49428.data.br",
+    "Build/ad0b89e48ad0c0ece13349fd9ff33df5.wasm.br",
     "TemplateData/style.css"
 
 ];
