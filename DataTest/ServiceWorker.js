f560afd2cf6bc9f62cd70ffe5b7e2f220ad31de2
Baste <assasinadyg@gmail.com>
2025-06-30 20:24:44 +0300

    diamond fall

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/eee9c96a3c841633dc3249971259cc33.loader.js",
-    "Build/fc6a08837b3826186f07d6c925e44558.framework.js.br",
-    "Build/891471d3529ae47fe051e29736d36a3c.data.br",
-    "Build/2a749376b2cf0a823eb52cab652acd3a.wasm.br",
+    "Build/2d15a625d0fc1c2f88de442389ae920b.loader.js",
+    "Build/67f4933f7aae5074881f071d9fbc034f.framework.js.br",
+    "Build/ec902508c2636b5d962c5bf853483c1b.data.br",
+    "Build/6281280d2f051a1f5a80b2d99737f372.wasm.br",
     "TemplateData/style.css"
 
 ];
