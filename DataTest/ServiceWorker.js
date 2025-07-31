c208fc7c8b10bc46ddf7538112a9ee6f885d9d6e
Baste <assasinadyg@gmail.com>
2025-07-31 18:58:08 +0300

    Bugfixes, customers walk

--- DataTest/ServiceWorker.js
+++ DataTest/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-motchi-1.0";
 const contentToCache = [
-    "Build/6cfa1f9205e17bd6ce6fa96d85086b89.loader.js",
-    "Build/31fbb62d8a4f4bb1c4968db5809862f7.framework.js.br",
-    "Build/9410a6fd7c2a913655475886aea9b158.data.br",
-    "Build/05aad9262215329a644269710e2214b0.wasm.br",
+    "Build/209600928a38f166117444172e4b5914.loader.js",
+    "Build/e8c5ad8f089f215fe48de9243e928b05.framework.js.br",
+    "Build/a54ab33a52c64a85552996486dd77f43.data.br",
+    "Build/e2298d5390c5732e4c40815c37f9def3.wasm.br",
     "TemplateData/style.css"
 
 ];
